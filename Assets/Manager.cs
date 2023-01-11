7376020c4ed0e794d37e42220a948a6a14f5011d
Samuel Charlet <s.ch4rl3t@gmail.com>
2023-01-11 16:49:04 +0100

    Chrono

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -7,9 +7,9 @@ using TMPro;
 public class Manager : MonoBehaviour
 {
     public static Manager Instance;
-    
-    [Header("Objects To Get")]
-    [SerializeField] private GameObject[] _objects;
+
+    [Header("Objects To Get")] [SerializeField]
+    private GameObject[] _objects;
 
     [Header("Chrono")] [SerializeField] private TextMeshProUGUI _chronoText;
     [SerializeField] private float _chrono;
@@ -25,6 +25,17 @@ public class Manager : MonoBehaviour
         {
             obj.SetActive(false);
         }
+    }
+
+    private void Update()
+    {
+        if (!(_chrono > 0)) return;
+        
+        _chrono -= Time.deltaTime;
+        TimeSpan time = TimeSpan.FromSeconds(_chrono);
+        var format = time.ToString(@"g");
+        format = format.Substring(5, 5);
+        _chronoText.text = format;
     }
 
     public void ObjectGet(int index)
